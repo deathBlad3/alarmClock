c792c8aeb3a0fb77f964645e475f3a495ae49825
deathBlad3 <amal.1293@gmail.com>
2016-07-18 14:55:07 +0530

    Added option to add new links

--- alarm.py
+++ alarm.py
@@ -4,30 +4,43 @@ import time
 from random import randint
 import webbrowser
 
+#TODO
+#Option to add new links
+
 parser = argparse.ArgumentParser()
 requiredNamed = parser.add_argument_group('required named arguments')
-requiredNamed.add_argument('-H', '--hour', help='Hour', action='store', dest='hour', type=int, required=True)
-requiredNamed.add_argument('-M','--minute', help='Minute', action='store', dest='minute', type=int, required=True)
-requiredNamed.add_argument('-t', help='AM/PM', action='store', dest='ap', required=True)
+requiredNamed.add_argument('-H', '--hour', help='Hour', action='store', dest='hour', type=int)
+requiredNamed.add_argument('-M','--minute', help='Minute', action='store', dest='minute', type=int)
+requiredNamed.add_argument('-t', help='AM/PM', action='store', dest='ap')
+requiredNamed.add_argument('-a','--add', help='Add New Link', action='store', dest='add')
 args = parser.parse_args()
 
 #Waits the specified amount of seconds and plays a random video on YouTube
 def playVideo(secondsNeeded):
     print "Alarm Set"
     time.sleep(secondsNeeded)
+    print "BEEP BEEP!"
     links = 'links.txt'
     with open(links) as f:
         linksList = f.read().splitlines()
     id = randint(0,len(linksList)-1)
     webbrowser.open(linksList[id])
+
+def addLink(link):
+    links = 'links.txt'
+    with open(links,'a') as f:
+        f.write(link+"\n")
     
-now = datetime.datetime.now()
-if args.ap=='PM':
-    args.hour+=12
-neededTime = now.replace(hour=args.hour,minute=args.minute,second=0,microsecond=0)
-if now > neededTime:
-    print "Its over man!"
+if args.add:
+    addLink(args.add)
 else:
-    secondsNeeded = int((neededTime-now).total_seconds())
-    playVideo(secondsNeeded)
-    
+    now = datetime.datetime.now()
+    if args.ap=='PM':
+        args.hour+=12
+    neededTime = now.replace(hour=args.hour,minute=args.minute,second=0,microsecond=0)
+    if now > neededTime:
+        print "Its over man!"
+    else:
+        secondsNeeded = int((neededTime-now).total_seconds())
+        playVideo(secondsNeeded)
+            
