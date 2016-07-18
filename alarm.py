b1c74632bf34582c32069ce41f354567ca4e5de0
deathBlad3 <amal.1293@gmail.com>
2016-07-18 12:45:34 +0530

    Calculated time difference to delay

--- alarm.py
+++ alarm.py
@@ -1,4 +1,6 @@
 import argparse
+import datetime
+import time
 
 parser = argparse.ArgumentParser()
 requiredNamed = parser.add_argument_group('required named arguments')
@@ -7,4 +9,17 @@ requiredNamed.add_argument('-M','--minute', help='Minute', action='store', dest=
 requiredNamed.add_argument('-t', help='AM/PM', action='store', dest='ap', required=True)
 args = parser.parse_args()
 
+#Waits the specified amount of seconds and plays a random video on YouTube
+def playVideo(secondsNeeded):
+    
 
+now = datetime.datetime.now()
+if args.ap=='PM':
+    args.hour+=12
+neededTime = now.replace(hour=args.hour,minute=args.minute,second=0,microsecond=0)
+if now > neededTime:
+    print "Its over man!"
+else:
+    secondsNeeded = int((neededTime-now).total_seconds())
+    playVideo(secondsNeeded)
+    
